959e238ce160ef1ca5391b1eac11e022cf7a6b0b
caren <carenoliveirasilvapontes@gmail.com>
2023-04-30 21:18:21 -0300

    feat:app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Form from './pages/Form';
 import Login from './pages/Login';
+import './App.css';
 
 function App() {
   return (
