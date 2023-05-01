dbfb0a48dedb32aa5880eeb0096994fd4acb4d57
caren <carenoliveirasilvapontes@gmail.com>
2023-04-30 21:17:58 -0300

    feat:login implementado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import AppContext from '../Context/AppContext';
+import './Login.css';
 
 export default function Login() {
   const { name, email, password,
@@ -8,40 +9,50 @@ export default function Login() {
   const history = useHistory();
 
   return (
-    <form>
-      <label htmlFor="email" className="flex ">
+    <section className="containerForm">
+      <form className="containerInputs">
+        <h3>Login</h3>
+        <label htmlFor="name" className="flex ">
 
-        <input
-          type="text"
-          placeholder="name"
-          value={ name }
-          onChange={ ({ target }) => setName(target.value) }
-        />
-      </label>
-      <label htmlFor="email" className="flex ">
+          <input
+            className="inputLogin"
+            id="name"
+            type="text"
+            placeholder="name"
+            value={ name }
+            onChange={ ({ target }) => setName(target.value) }
+          />
+        </label>
+        <label htmlFor="email" className="flex ">
 
-        <input
-          type="email"
-          placeholder="email"
-          value={ email }
-          onChange={ ({ target }) => setEmail(target.value) }
-        />
-      </label>
-      <label htmlFor="password" className="flex">
+          <input
+            className="inputLogin"
+            id="email"
+            type="email"
+            placeholder="email"
+            value={ email }
+            onChange={ ({ target }) => setEmail(target.value) }
+          />
+        </label>
+        <label htmlFor="password" className="flex">
 
-        <input
-          type="password"
-          placeholder="password"
-          value={ password }
-          onChange={ ({ target }) => setPassword(target.value) }
-        />
-      </label>
-      <button
-        type="button"
-        onClick={ () => history.push('/form') }
-      >
-        Enter
-      </button>
-    </form>
+          <input
+            className="inputLogin"
+            id="password"
+            type="password"
+            placeholder="password"
+            value={ password }
+            onChange={ ({ target }) => setPassword(target.value) }
+          />
+        </label>
+        <button
+          className="enter"
+          type="button"
+          onClick={ () => history.push('/form') }
+        >
+          Enter
+        </button>
+      </form>
+    </section>
   );
 }
