ca6dff9cbd912f4f24ac7ffadf67a33348b7d220
caren <carenoliveirasilvapontes@gmail.com>
2023-05-01 12:27:34 -0300

    feat:login para iniciar os states

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './Login.css';
 
 export default function Login() {
+  const history = useHistory();
+
   return (
-    <h1>Login</h1>
+    <section className="containerForm">
+      <form className="containerInputs">
+        <h3>Login</h3>
+        <label htmlFor="name" className="flex ">
+
+          <input
+            className="inputLogin"
+            id="name"
+            type="text"
+            placeholder="name"
+            value="name"
+          />
+        </label>
+        <label htmlFor="email" className="flex ">
+
+          <input
+            className="inputLogin"
+            id="email"
+            type="email"
+            placeholder="email"
+            value="email"
+
+          />
+        </label>
+        <label htmlFor="password" className="flex">
+
+          <input
+            className="inputLogin"
+            id="password"
+            type="password"
+            placeholder="password"
+            value="password"
+
+          />
+        </label>
+        <button
+          className="enter"
+          type="button"
+          onClick={ () => history.push('/form') }
+        >
+          Enter
+        </button>
+      </form>
+    </section>
   );
 }
