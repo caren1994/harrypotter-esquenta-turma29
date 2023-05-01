e694f79b12168c9ef132f882bd18d836e79ecf44
caren <carenoliveirasilvapontes@gmail.com>
2023-04-30 21:18:49 -0300

    feat:table concluido

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,46 +1,50 @@
 import React, { useContext } from 'react';
 import AppContext from '../Context/AppContext';
+import './Table.css';
 
 export default function Table() {
-  const { data } = useContext(AppContext);
+  const { data, inputText } = useContext(AppContext);
 
   return (
-    <table className="tabela">
-      <thead>
-        <tr>
-          <th>Name</th>
+    <div className="divmaster">
+      <table className="tabela">
+        <thead className="head">
+          <tr className="tr">
+            <th className="th">Name</th>
 
-          <th>species</th>
+            <th className="th">species</th>
 
-          <th>gender</th>
+            <th className="th">gender</th>
 
-          <th>house</th>
+            <th className="th">house</th>
 
-          <th>yearOfBirth</th>
+            <th className="th">yearOfBirth</th>
 
-          <th>alternate Names</th>
+            <th className="th">alternate Names</th>
 
-          <th>image</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((e) => (
-          <tr
-            key={ e.id }
-          >
-            <td>{ e.name }</td>
-            <td>{e.species}</td>
-            <td>{e.gender}</td>
-            <td>{e.house}</td>
-            <td>{e.yearOfBirth}</td>
-            <td>
-              {e.alternate_names?.map((w, index) => <span key={ index }>{w}</span>)}
+            <th className="th">image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.filter((e) => (e.name.includes(inputText))).map((e) => (
+            <tr
+              className="tr"
+              key={ e.id }
+            >
+              <td className="td">{ e.name }</td>
+              <td>{e.species}</td>
+              <td>{e.gender}</td>
+              <td>{e.house}</td>
+              <td>{e.yearOfBirth}</td>
+              <td>
+                {e.alternate_names.map((w, index) => <span key={ index }>{w}</span>)}
 
-            </td>
-            <td>{e.image}</td>
-          </tr>))}
+              </td>
+              <td>{e.image}</td>
+            </tr>))}
 
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
