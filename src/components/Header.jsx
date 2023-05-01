2d97fa732cbddd1f1f2c8b8ebf4662cf5ad2f387
caren <carenoliveirasilvapontes@gmail.com>
2023-04-30 21:18:35 -0300

    feat:header concluido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../Context/AppContext';
+import './Header.css';
 
 export default function Header() {
   const { name } = useContext(AppContext);
   return (
-    <nav>
-      <h3>{name}</h3>
-      <Link to="/">Login</Link>
+    <nav className="containerNav">
+      <div className="containerhl">
+        <h3 className="h3">{name}</h3>
+        <Link className="link" to="/">Login</Link>
+
+      </div>
+      <div>
+        <h2 className="nameHarry">Harry Potter</h2>
+      </div>
     </nav>
   );
 }
