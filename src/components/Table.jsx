b99a9c4423d47901fe5466e8767952ff4fa2cc79
caren <carenoliveirasilvapontes@gmail.com>
2023-05-01 12:28:00 -0300

    feat:table para iniciar

---
+++ src/components/Table.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Table.css';
+
+export default function Table() {
+  return (
+    <div className="divmaster">
+      <table className="tabela">
+        <thead className="head">
+          <tr className="tr">
+            <th className="th"> projeto</th>
+
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr
+            className="tr"
+
+          >
+            <td className="td">iniciando o projeto</td>
+
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+  );
+}
