1d36702851c2d174060a36c7e7c3df2c18e313e3
caren <carenoliveirasilvapontes@gmail.com>
2023-05-02 08:22:43 -0300

    feat:fetch comentado

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import './Table.css';
 
 export default function Table() {
+  // useEffect(() => {
+  //   const requestApi = async () => {
+  //     const response = await fetch('https://hp-api.onrender.com/api/characters');
+  //     const result = await response.json();
+  //   };
+  //   requestApi();
+  // }, []);
   return (
     <div className="divmaster">
       <table className="tabela">
